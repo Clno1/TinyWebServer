0589d6d8eb807e3d0019583469dc43ad62e6f8bd
Clno1 <35722297+Clno1@users.noreply.github.com>
2020-05-26 21:42:26 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,18 +6,27 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include <assert.h>
-#include <strings.h>
+#include <string.h>
 #include<sys/epoll.h>
 #include <errno.h>
+#include <fcntl.h>
+#include<signal.h>
 
 #include "epoll/epoll.h"
 #include "http/http_conn.h"
+#include"timer/lst_timer.h"
+
 
 #define MAX_FD 65536        //最大的文件描述符数量
 #define MAX_EVENT_NUMBER 10000      //最大事件数量
 #define TIMESLOT 5          //时间间隔
 
+int epollfd,pipefd[2];
+sort_timer_lst timer_lst;       //定时器链表
 int listen_init(int port);
+int timer_init(int epollfd,int *pipefd);
+void cb_func(client_data* user_data);
+void timer_handler();
 
 int main(int argc,char *argv[])
 {
@@ -31,12 +40,17 @@ int main(int argc,char *argv[])
     http_conn::m_epollfd = epollfd;
     epoll_event events[MAX_EVENT_NUMBER];
 
+    
+    timer_init(epollfd,pipefd);     //定时器初始工作
+
     addfd(epollfd,listenfd,false);      //把listenfd添加到监听红黑树上
 
     http_conn* users = new http_conn[MAX_FD];
     assert(users);
 
     bool stop_server = false;
+    bool timeout = false;       //定时时间是否到了
+    alarm(TIMESLOT);        //定下第一个闹钟
     while (!stop_server)
     {
         int total = epoll_wait(epollfd,events,MAX_EVENT_NUMBER,-1);
@@ -57,17 +71,18 @@ int main(int argc,char *argv[])
                 //LT
                 int connfd = accept(listenfd, (struct sockaddr*)&client_address, &client_address_len);
                 if (connfd < 0) {
-                    //
+                    //accept错误
                     continue;
                 }
-                /*if (http_conn::m_user_conunt >= MAX_FD) {
-
-                }*/
+                if (http_conn::m_user_count >= MAX_FD) {
+                    //用户数量超过最大描述符了
+                    continue;
+                }
 
                 users[connfd].init(connfd, client_address);     // 初始化新客户，并epoll监听
 
             }
-            //
+            //对端关闭连接
             else if (whatopt & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) {
 
             }
@@ -82,6 +97,11 @@ int main(int argc,char *argv[])
             else if (whatopt & EPOLLOUT) {
 
             }
+        }
+
+        if (timeout) {
+            timer_handler();
+            timeout = false;
         }
     }
 
@@ -111,4 +131,53 @@ int listen_init(int port) {
     assert(listen(listenfd, 20) >= 0);
     
     return listenfd;
+}
+//定时器的回调函数（执行函数）
+//函数作用就是把这个客户关闭（长时间无反应）
+void cb_func(client_data* user_data) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, user_data->sockfd, 0);
+    assert(user_data);
+    close(user_data->sockfd);
+    http_conn::m_user_count--;
+}
+//注册sig信号的回调函数为handler
+void addsig(int sig, void (handler)(int), bool restart = true) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handler;
+    if (restart)
+        sa.sa_flags |= SA_RESTART;
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+//把文件描述符fd设为非阻塞
+int setnoblocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+//信号处理（回调）函数
+void sig_handler(int sig) {
+    //为保证函数的可重入性，保留原来的errno
+    int save_errno = errno;
+    int msg = sig;
+    send(pipefd[1], (char*)&msg, 1, 0);
+    errno = save_errno;
+}
+//设置定时器前的小工作
+int timer_init(int epollfd,int* pipefd) {
+    int ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
+    assert(ret != -1);
+    setnoblocking(pipefd[1]);       //设置管道写端为非阻塞，防止缓冲区满了
+    addfd(epollfd, pipefd[0], false);   //设置管道读端为ET非阻塞
+
+    addsig(SIGALRM, sig_handler, false);
+    addsig(SIGTERM, sig_handler, false);
+    return ret;
+}
+//定时处理任务，重新定时以不断触发SIGALRM信号
+void timer_handler() {
+    timer_lst.tick();       //时间到咯，开始清理
+    alarm(TIMESLOT);        //定下下一个时间
 }
---
+++ timer/lst_timer.h
@@ -0,0 +1,167 @@
+#ifndef LST_TIMER
+#define LST_TIMER
+
+#include<time.h>
+#include <netinet\in.h>
+
+class util_timer;
+struct client_data
+{
+	sockaddr_in address;
+	int sockfd;
+	util_timer* timer;
+};
+
+//链表结点，包含事件和客户数据
+class util_timer
+{
+public:
+	util_timer() : prev(NULL),next(NULL) {}
+
+public:
+	time_t expire;		//记录时间
+	//!!!定时器的执行函数，到时间就调用这个
+	void (*cb_func)(client_data*);	
+	client_data* user_data;		//客户数据
+	util_timer* prev;		//双向链表
+	util_timer* next;		//双向链表
+};
+
+
+//链表，按事件升序排序
+class sort_timer_lst
+{
+public:
+	//链表的构造与析构函数
+	sort_timer_lst() : head(NULL), tail(NULL) {};
+	~sort_timer_lst() {
+		util_timer* tmp = head;
+		while (tmp) {
+			head = tmp->next;
+			delete tmp;
+			tmp = head;
+		}
+	}
+	//插入结点
+	void add_timer(util_timer* timer) {
+		if (!timer) return;
+		if (!head) {
+			head = tail = timer;
+			return;
+		}
+		//如果新的定时器超时时间小于当前头部结点
+		//直接将当前定时器结点作为头部结点
+		if (timer->expire < head->expire) {
+			timer->next = head;
+			head->next = timer;
+			head = timer;
+			return;
+		}
+
+		//至少不是插入到头，调用函数继续插入
+		add_timer(timer, head);
+	}
+	//调整定时器，任务发生变化时，调整定时器在链表中的位置
+	void adjust_timer(util_timer* timer) {
+		if (!timer) return;
+		util_timer* tmp = timer->next;
+
+		//因为只会增加，所以如果在最后肯定无需调整
+		if (!tmp || (timer->expire < tmp->expire)) return;
+
+		//分两种情况：头/非头。思路都是先删除，再调用插入函数重新插入
+		if (timer == head) {
+			head = head->next;
+			head->prev = NULL;
+			timer->next = NULL;
+			add_timer(timer, head);
+		}
+		else {
+			timer->prev->next = timer->next;
+			timer->next->prev = timer->prev;
+			add_timer(timer, timer->next);
+		}
+	}
+	//删除定时器
+	void del_timer(util_timer* timer) {
+		if (!timer) return;
+		//即整个链表就剩下一个结点，直接删除
+		if (timer == head && timer == tail) {
+			delete timer;
+			head = NULL;
+			tail = NULL;
+			return;
+		}
+		//被删除的定时器为头结点
+		if (timer == head) {
+			head = head->next;
+			head->prev = NULL;
+			delete timer;
+			return;
+		}
+		//被删除的是尾结点
+		if (timer == tail) {
+			tail->prev = tail;
+			tail->next = NULL;
+			delete tail;
+			return;
+		}
+		//不是头尾，普通删除
+		timer->prev->next = timer->next;
+		timer->next->prev = timer->prev;
+		delete timer;
+		return;
+	}
+	//定时任务处理函数
+	void tick() {
+		if (!head) return;
+		time_t cur = time(NULL);		//获取当前时间
+		util_timer* tmp = head;
+		while (tmp)
+		{
+			if (cur < tmp->expire) break;		//就到这里了，后面的执行时间都还没到
+			tmp->cb_func(tmp->user_data);
+			//执行完之后，删除链表头并移动头
+			head = tmp->next;
+			if (head)
+				head->prev = NULL;
+			delete tmp;
+			tmp = head;
+		}
+	}
+
+private:
+	//把timer插入到链表中，经过上面的检测到这里至少不是插入到头
+	void add_timer(util_timer* timer, util_timer* lst_head) {
+		util_timer* prev = lst_head;
+		util_timer* tmp = prev->next;
+		//遍历当前结点之后的链表，按照超时时间找到目标定时器对应的位置，常规双向链表插入操作
+		while (tmp)
+		{
+			//插入到prev后，tmp之前
+			if (timer->expire < tmp->expire) {
+				prev->next = timer;
+				timer->next = tmp;
+				tmp->prev = timer;
+				timer->prev = prev;
+				break;
+			}
+			prev = tmp;
+			tmp = tmp->next;
+		}
+		//上面没有插入成功，证明要插入到最后面
+		if (!tmp) {
+			prev->next = timer;
+			timer->prev = prev;
+			timer->next = NULL;
+			tail = timer;
+		}
+	}
+
+private:
+	util_timer* head;
+	util_timer* tail;
+};
+
+
+#endif
